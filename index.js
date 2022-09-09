ae9aebc598da48cb508b2f9d9b1110b39dacc641
Evans Ochieng <evansochieng121@gmail.com>
2022-09-09 23:34:27 +0300

    started on class inheritance

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+
+// Geometry application
+class Polygon {
+    constructor(Array){
+        this.sides = Array;
+    }
+
+    // count sides
+    get countSides(){
+        return this.sides.length;
+    }
+
+    // parameter
+    get parameter(){
+        const total = this.sides.reduce((sum, side) => sum + side, 0);
+        return total;
+    }
+}
+
+// class triangle
+class Triangle extends Polygon {
+    // constructor(sides){
+    //     super(sides);
+    // }
+
+    // check if the triangle is valid
+    // sum of pairs of sides should be > third side
+    get isValid(){
+        const sides = this.sides;
+        
+    }
+}
