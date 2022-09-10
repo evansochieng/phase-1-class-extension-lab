e48a1b5c3d98ce6def7f159aa7d87ab6229ea307
Evans Ochieng <evansochieng121@gmail.com>
2022-09-10 15:06:56 +0300

    Completed class extension exercise

--- index.js
+++ index.js
@@ -12,22 +12,33 @@ class Polygon {
     }
 
     // parameter
-    get parameter(){
-        const total = this.sides.reduce((sum, side) => sum + side, 0);
-        return total;
+    get perimeter(){
+        return this.sides.reduce((sum, side) => sum + side, 0);
     }
 }
 
 // class triangle
 class Triangle extends Polygon {
-    // constructor(sides){
-    //     super(sides);
-    // }
-
     // check if the triangle is valid
     // sum of pairs of sides should be > third side
     get isValid(){
-        const sides = this.sides;
-        
+        //const sides = this.sides;
+        const check1 = this.sides[0] + this.sides[1] > this.sides[2];
+        const check2 = this.sides[0] + this.sides[2] > this.sides[1];
+        const check3 = this.sides[1] + this.sides[2] > this.sides[0];
+        return ( check1 && check2 && check3 );
+    }
+}
+
+// class Square
+class Square extends Polygon {
+    // check if all sides are equal
+    get isValid(){
+        return (this.sides[0] === this.sides[1]) && (this.sides[2] === this.sides[3]);
+    }
+
+    // area method
+    get area(){
+        return this.sides[0] * this.sides[0];
     }
 }
